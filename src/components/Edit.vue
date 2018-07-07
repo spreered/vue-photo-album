1ca32b2a958b67d0e4e9da3b2bd5d56999778c42
spreered <spreered@gmail.com>
2018-07-07 16:25:33 +0800

    Edit remove api

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="edit">
     <div class="photo-container">
-      <ImgDisplay :url="'http://35.185.111.183'+photo.file_location.url" v-if="photo.file_location"/>
+      <!-- <ImgDisplay :url="'http://35.185.111.183'+photo.file_location.url" v-if="photo.file_location"/> -->
+      <ImgDisplay/>
     </div>
 
     <div class="main-container">
-      <PhotoForm
+      <!-- <PhotoForm
       :title="photo.title"
       :description="photo.description"
       v-if="photo.id"
-      @photo-form-submit="patchUpdate"/> 
+      @photo-form-submit="patchUpdate"/>  -->
+      <PhotoForm/>
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+  // import axios from 'axios'
   import ImgDisplay from '@/components/ImgDisplay'
   import PhotoForm from '@/components/PhotoForm'
   export default {
@@ -28,58 +30,58 @@
         photo: {},
       }
     },
-    methods: {
-      patchUpdate: function(payload){
-        const id = this.$route.params.id
-        const updateUrl = 'http://35.185.111.183/api/v1/photos/'+id
-        const token = JSON.parse(localStorage.getItem('photo-album-user')).authToken
-        let params = new FormData();
-        params.append('auth_token',token),
-        params.append('title',payload.title),
-        params.append('date',payload.date),
-        params.append('description',payload.description),
-        params.append('file_location',payload.file_location)
+    // methods: {
+    //   patchUpdate: function(payload){
+    //     const id = this.$route.params.id
+    //     const updateUrl = 'http://35.185.111.183/api/v1/photos/'+id
+    //     const token = JSON.parse(localStorage.getItem('photo-album-user')).authToken
+    //     let params = new FormData();
+    //     params.append('auth_token',token),
+    //     params.append('title',payload.title),
+    //     params.append('date',payload.date),
+    //     params.append('description',payload.description),
+    //     params.append('file_location',payload.file_location)
 
-        let that = this;
-        console.log(params)
-        axios.patch(updateUrl, params,
-        {
-          headers: {
-              'Content-Type': 'multipart/form-data'
-          }
-        })
-          .then(function(res) {
-             console.log(res);
-             that.$router.push('/photo/'+ res.data.result.id)
+    //     let that = this;
+    //     console.log(params)
+    //     axios.patch(updateUrl, params,
+    //     {
+    //       headers: {
+    //           'Content-Type': 'multipart/form-data'
+    //       }
+    //     })
+    //       .then(function(res) {
+    //          console.log(res);
+    //          that.$router.push('/photo/'+ res.data.result.id)
              
-          })
-          .catch(function(err) { console.error(err) })
-      },
-    },
-    created(){
-    //   console.log('edit');
-      const id = this.$route.params.id
-      const url = 'http://35.185.111.183/api/v1/photos/' + id
-      const token = JSON.parse(localStorage.getItem('photo-album-user')).authToken
-      const params = { auth_token: token }
-      const that = this
-      axios.get(url, { params })
-        .then(function(res) {
-          // {
-          //   "id": 1,
-          //   "title": "Myra's Birthday",
-          //   "description": "This is my favourite photo",
-          //   "file_location": {
-          //     "url": "/uploads/photo/file_location/1/myra-birthday.png"
-          //   }
-          // }
-          that.photo = res.data
-          // console.dir(that.data)
-        })
-        .catch(function(err) {
-          console.dir(err)
-        })
-    }
+    //       })
+    //       .catch(function(err) { console.error(err) })
+    //   },
+    // },
+    // created(){
+    // //   console.log('edit');
+    //   const id = this.$route.params.id
+    //   const url = 'http://35.185.111.183/api/v1/photos/' + id
+    //   const token = JSON.parse(localStorage.getItem('photo-album-user')).authToken
+    //   const params = { auth_token: token }
+    //   const that = this
+    //   axios.get(url, { params })
+    //     .then(function(res) {
+    //       // {
+    //       //   "id": 1,
+    //       //   "title": "Myra's Birthday",
+    //       //   "description": "This is my favourite photo",
+    //       //   "file_location": {
+    //       //     "url": "/uploads/photo/file_location/1/myra-birthday.png"
+    //       //   }
+    //       // }
+    //       that.photo = res.data
+    //       // console.dir(that.data)
+    //     })
+    //     .catch(function(err) {
+    //       console.dir(err)
+    //     })
+    // }
   }
 </script>
 
